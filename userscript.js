46729f0ae088f227551707c84d282f90cd922da7
Tearat <raiiliiat@yandex.ru>
2023-05-04 14:09:54 +0500

    Add top rated arts section

--- userscript.js
+++ userscript.js
@@ -9,54 +9,102 @@
 // @grant        none
 // ==/UserScript==
 
-(function() {
-    'use strict';
-    function parseNumber(node) {
-        let text = node.split('</i>')[1]
-        const isMoreThanThousand = text.indexOf('K') > -1
-        let number = parseInt(text)
-        return isMoreThanThousand ? number * 1000 : number
-    }
+;(function () {
+  'use strict'
 
-    function renderStats(totalViews, totalLikes, totalComments) {
-        const portfolioContentNode = document.getElementsByClassName('portfolio-content')[0]
-        const projectListNode = document.getElementsByClassName('project-list')[0]
-        const statDiv = document.createElement("div")
-        const viewsToLikeConversion = Math.floor(totalLikes / totalViews * 100)
-        const viewsToCommentConversion = Math.floor(totalComments / totalViews * 100)
-        statDiv.innerHTML += `<h2>General statistics:</h2>`
-        statDiv.innerHTML += `Views: ${totalViews} <br> Likes: ${totalLikes} <br> Comments: ${totalComments} <br>`
-        statDiv.innerHTML += `Views-to-like conversion: ${viewsToLikeConversion}% <br> Views-to-comment conversion: ${viewsToCommentConversion}%`
-        portfolioContentNode.insertBefore(statDiv, projectListNode)
-    }
+  function parseNumber(node) {
+    let text = node.split('</i>')[1]
+    const isMoreThanThousand = text.indexOf('K') > -1
+    let number = parseFloat(text)
+    return isMoreThanThousand ? number * 1000 : number
+  }
 
-    function addConversions() {
-        const metas = document.getElementsByClassName('project-meta')
-        let totalViews = 0
-        let totalLikes = 0
-        let totalComments = 0
-        for (var i = 0; i < metas.length; i++) {
-            const meta = metas[i]
-            let views = parseNumber(meta.childNodes[0].innerHTML)
-            if(!meta.childNodes[1]) continue;
-            const likes = parseNumber(meta.childNodes[1].innerHTML)
-            totalViews += views
-            totalLikes += likes
-            if(meta.childNodes[2]) {
-                const comments = parseNumber(meta.childNodes[2].innerHTML)
-                totalComments += comments
-            }
-            const conversion = Math.floor(likes / views * 100)
-            const newChild = document.createElement("div")
-            newChild.classList.add("project-meta-item");
-            newChild.innerHTML = `${conversion}%`
-            newChild.style.color = 'limegreen'
-            meta.appendChild(newChild)
-        }
-        renderStats(totalViews, totalLikes, totalComments)
+  function extractArtData(art) {
+    const title = art.getElementsByClassName('project-title')[0].innerHTML
+    const link = art.getElementsByClassName('project-link')[0].getAttribute('href').split('/')[3]
+    const meta = art.getElementsByClassName('project-meta')[0]
+    const views = parseNumber(meta.childNodes[0].innerHTML)
+    const likes = meta.childNodes[1] ? parseNumber(meta.childNodes[1].innerHTML) : 0
+    const comments = meta.childNodes[2] ? parseNumber(meta.childNodes[2].innerHTML) : 0
+    const conversion = Math.floor((likes / views) * 100)
+    return {
+      title,
+      link,
+      views,
+      likes,
+      comments,
+      conversion,
+      meta,
     }
+  }
 
-    window.addEventListener("load", (event) => {
-        addConversions()
-    });
-})();
+  function renderStats(arts) {
+    const totalViews = arts.reduce((acc, art) => acc + art.views, 0)
+    const totalLikes = arts.reduce((acc, art) => acc + art.likes, 0)
+    const totalComments = arts.reduce((acc, art) => acc + art.comments, 0)
+
+    let mostViewed = [...arts.sort((a, b) => b.views - a.views).slice(0, 5)]
+    let mostLiked = [...arts.sort((a, b) => b.likes - a.likes).slice(0, 5)]
+    let mostCommented = [...arts.sort((a, b) => b.comments - a.comments).slice(0, 5)]
+
+    const portfolioContentNode = document.getElementsByClassName('portfolio-content')[0]
+    const projectListNode = document.getElementsByClassName('project-list')[0]
+    const statDiv = document.createElement('div')
+    const viewsToLikeConversion = Math.floor((totalLikes / totalViews) * 100)
+    const viewsToCommentConversion = Math.floor((totalComments / totalViews) * 100)
+    statDiv.innerHTML += `<h2>General statistics:</h2>`
+    statDiv.innerHTML += `Views: ${totalViews} <br> Likes: ${totalLikes} <br> Comments: ${totalComments} <br>`
+    statDiv.innerHTML += `Views-to-like conversion: ${viewsToLikeConversion}% <br> Views-to-comment conversion: ${viewsToCommentConversion}% <br>`
+
+    statDiv.innerHTML +=
+      `Most viewed arts: ` +
+      mostViewed
+        .map(
+          (art) =>
+            `<a href='https://www.artstation.com/artwork/${art.link}' target='_blank'>${art.title}</a> (${art.views} views)`
+        )
+        .join(', ') +
+      `<br>`
+    statDiv.innerHTML +=
+      `Most liked arts: ` +
+      mostLiked
+        .map(
+          (art) =>
+            `<a href='https://www.artstation.com/artwork/${art.link}' target='_blank'>${art.title}</a> (${art.likes} likes)`
+        )
+        .join(', ') +
+      `<br>`
+    statDiv.innerHTML +=
+      `Most commented arts: ` +
+      mostCommented
+        .map(
+          (art) =>
+            `<a href='https://www.artstation.com/artwork/${art.link}' target='_blank'>${art.title}</a> (${art.comments} comments)`
+        )
+        .join(', ') +
+      `<br>`
+
+    portfolioContentNode.insertBefore(statDiv, projectListNode)
+  }
+
+  function addConversions() {
+    const projectNodes = document.getElementsByClassName('project-inner')
+    const arts = []
+    for (var n = 0; n < projectNodes.length; n++) {
+      arts.push(extractArtData(projectNodes[n]))
+    }
+    for (var i = 0; i < arts.length; i++) {
+      const art = arts[i]
+      const newChild = document.createElement('div')
+      newChild.classList.add('project-meta-item')
+      newChild.innerHTML = `${art.conversion}%`
+      newChild.style.color = 'limegreen'
+      art.meta.appendChild(newChild)
+    }
+    renderStats(arts)
+  }
+
+  window.addEventListener('load', (event) => {
+    addConversions()
+  })
+})()
